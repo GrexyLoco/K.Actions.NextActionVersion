69a5da24bd638bfa7ae51fe1283574a8069d32cf
1d70f <GrexyLoco@users.noreply.github.com>
2025-08-19 02:41:45 +0200

    Create PowerShell module manifest for NextActionVersion

---
+++ NextActionVersion.psm1
@@ -0,0 +1,19 @@
+#
+# NextActionVersion.psm1
+# 
+# PowerShell module for GitHub Actions version management
+# Uses Git tags as single source of truth for semantic versioning
+#
+
+# Import helper functions
+. $PSScriptRoot\Functions\Private\Get-GitTags.ps1
+. $PSScriptRoot\Functions\Private\Get-CommitsSinceTag.ps1
+. $PSScriptRoot\Functions\Private\Get-BumpTypeFromCommits.ps1
+. $PSScriptRoot\Functions\Private\Step-SemanticVersion.ps1
+. $PSScriptRoot\Functions\Private\New-ActionVersionResult.ps1
+. $PSScriptRoot\Functions\Public\Get-NextActionVersion.ps1
+
+# Export public functions
+Export-ModuleMember -Function @(
+    'Get-NextActionVersion'
+)
