c6a24d2bd5dbb507a39f0c4654824fc6d7d0d3bd
1d70f <GrexyLoco@users.noreply.github.com>
2025-08-19 02:44:12 +0200

    Create New-ActionVersionResult helper function

---
+++ Functions/Private/New-ActionVersionResult.ps1
@@ -0,0 +1,58 @@
+function New-ActionVersionResult {
+    <#
+    .SYNOPSIS
+        Creates standardized result object for NextActionVersion
+    
+    .DESCRIPTION
+        Creates a consistent result object with all required properties
+        for GitHub Actions integration
+    
+    .OUTPUTS
+        PSCustomObject with standardized properties
+    #>
+    
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$CurrentVersion,
+        
+        [Parameter(Mandatory = $true)]
+        [string]$BumpType,
+        
+        [Parameter(Mandatory = $true)]
+        [string]$NewVersion,
+        
+        [Parameter(Mandatory = $true)]
+        [string]$LastReleaseTag,
+        
+        [Parameter(Mandatory = $true)]
+        [string]$TargetBranch,
+        
+        [Parameter(Mandatory = $false)]
+        [string]$Suffix = "",
+        
+        [Parameter(Mandatory = $false)]
+        [string]$Warning = "",
+        
+        [Parameter(Mandatory = $false)]
+        [bool]$ActionRequired = $false,
+        
+        [Parameter(Mandatory = $false)]
+        [string]$ActionInstructions = "",
+        
+        [Parameter(Mandatory = $false)]
+        [bool]$IsFirstRelease = $false
+    )
+    
+    return [PSCustomObject]@{
+        CurrentVersion = $CurrentVersion
+        BumpType = $BumpType
+        NewVersion = $NewVersion
+        LastReleaseTag = $LastReleaseTag
+        TargetBranch = $TargetBranch
+        Suffix = $Suffix
+        Warning = $Warning
+        ActionRequired = $ActionRequired
+        ActionInstructions = $ActionInstructions
+        IsFirstRelease = $IsFirstRelease
+    }
+}
